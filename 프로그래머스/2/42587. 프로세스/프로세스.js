4876d441ea0009adafbce20df16797121d0373ce
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-05-21 16:46:16 +0900

    [level 2] Title: 프로세스, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,17 @@
 function solution(priorities, location) {
-    let max = Math.max(...priorities);
-    let arr = Array.from(Array(priorities.length), (_, i) => i);
+    const arr = Array.from(Array(priorities.length), (_, i) => i);
+    
+    let max = 0;
     let answer = 0;
-    while(priorities.length != 0){
-         max = Math.max(...priorities);
-        
-        if(priorities[0] < max){
-            priorities.push(priorities.shift());
+    while(priorities.length !== 0) {
+        max = Math.max(...priorities);
+        if(priorities[0] < max) {
+            priorities.push(priorities.shift())
             arr.push(arr.shift());
-        }else {
-            answer+=1;
+        } else {
+            answer++;
             priorities.shift();
-            if(arr.shift() == location)
-                return answer;
+            if(arr.shift() === location) return answer;
         }
     }
 }
