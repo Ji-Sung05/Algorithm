8e41e7e9d00cac198dcc482256358b3c6864815e
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-07-03 16:08:21 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 316 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.js
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.js
@@ -5,32 +5,30 @@ let answerCount = 1
 let answer = ''
 
 while(N !== 0) {
-  const map = input.splice(0, N).map((str) => str.split(' ').map(Number))
-  dijkstra([0, 0], map)
-  N = +input.shift()
+    const map = input.splice(0, N).map(str => str.split(' ').map(Number))
+    dijkstra([0, 0], map)
+    N = +input.shift()
 }
 
 function dijkstra(start, map) {
-  const dx = [-1, 0, 1, 0]
-  const dy = [0, 1, 0, -1]
-  const [x, y] = start
-  const d = Array.from({ length: N }, () => Array(N).fill(Infinity))
+    const [x, y] = start
+    const dx = [-1, 0, 1, 0]
+    const dy = [0, 1, 0, -1]
+    const q = [[x, y]]
 
-  const q = [[x, y]]
-  let front = 0
-  d[y][x] = map[y][x]
-
-  while(q.length > front) {
-    const [x, y] = q[front++]
-    for(let i = 0; i < 4; i++) {
-      const [nx, ny] = [x + dx[i], y + dy[i]]
-      if(ny < 0 || nx < 0 || nx >= N || ny >= N) continue
-      if(d[ny][nx] > d[y][x] + map[ny][nx]) {
-        d[ny][nx] = d[y][x] + map[ny][nx]
-        q.push([nx, ny])
-      }
+    const d = Array.from({ length: N }, () => Array(N).fill(Infinity))
+    d[y][x] = map[y][x]
+    while(q.length > 0) {
+        const [x, y] = q.shift()
+        for(let i = 0; i < 4; i++) {
+            const [nx, ny] = [x + dx[i], y + dy[i]]
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue
+            if(d[ny][nx] > d[y][x] + map[ny][nx]) {
+                d[ny][nx] = d[y][x] + map[ny][nx]
+                q.push([nx, ny])
+            }
+        }
     }
-  }
-  answer += `Problem ${answerCount++}: ${d[N-1][N-1]}\n`
+    answer += `Problem ${answerCount++}: ${d[N-1][N-1]}\n`
 }
 console.log(answer)
