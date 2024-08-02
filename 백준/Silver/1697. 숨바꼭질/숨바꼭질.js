d28addc83e83823c9f56fbee99ba5ae18f5bee4b
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-08-02 13:51:41 +0900

    [Silver I] Title: 숨바꼭질, Time: 3444 ms, Memory: 20376 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,27 +1,35 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(v => +v);
+/*
+1. 아이디어
+1차원 배열에서 bfs를 푸는 문제
+사방위로 움직이는 것 대신 x-1, x+1, 2*x 위치로 이동하면서 동생의 위치와 같으면 몇 번 만에 찾았는지 출력
+2. 시간복잡도
+1차원 배열 O(N)
+3. 자료구조
+1차원 배열 O(N)
+*/
 
-const [N, K] = input;
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
 
-function bfs(start, end) {
-  const max = 100000;
-  const visited = Array(max + 1).fill(false);
-  const queue = [[start, 0]];
+const [N, K] = input.shift().split(' ').map(Number)
+const max = 100000
+const visited = Array(max + 1).fill(false)
 
-  while (queue.length > 0) {
-    const [current, time] = queue.shift();
+console.log(bfs())
 
-    if (current === end) {
-      return time;
+function bfs() {
+  const q = [[N, 0]]
+  visited[N] = true
+  
+  while(q.length !== 0) {
+    const [x, cnt] = q.shift()
+    if(x === K) {
+      return cnt
     }
-
-    for (const next of [current - 1, current + 1, current * 2]) {
-      if (next >= 0 && next <= max && !visited[next]) {
-        visited[next] = true;
-        queue.push([next, time + 1]);
+    for(const tar of [x + 1, x - 1, 2 * x]) {
+      if(tar >= 0 && tar <= max && !visited[tar]) {
+        visited[tar] = true
+        q.push([tar, cnt + 1])
       }
     }
   }
 }
-
-const result = bfs(N, K);
-console.log(result);
