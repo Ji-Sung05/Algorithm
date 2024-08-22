50182a02c5281e5de991ccef7cdd66ef4bdefcae
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-08-22 20:28:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1132 ms, Memory: 99500 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.js
@@ -0,0 +1,31 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [N, M, R] = input.shift().split(' ').map(Number)
+const arr = input.map(str => str.split(' ').map(Number))
+
+const graph = Array.from({ length: N + 1 }, () => [])
+arr.forEach(([a, b]) => {
+  graph[a].push(b)
+  graph[b].push(a)
+})
+graph.forEach(v => v.sort((a, b) => b - a))
+const visited = Array.from({ length: N + 1 }, () => 0)
+bfs(R)
+
+function bfs(s) {
+  const q = [s]
+  visited[s] = 1
+  let step = 1
+
+  while(q.length !== 0) {
+    const s = q.shift()
+    for(const d of graph[s]) {
+      if(!visited[d]) {
+        visited[d] = ++step
+        q.push(d)
+      }
+    }
+  }
+}
+
+console.log(visited.slice(1).join('\n'))
